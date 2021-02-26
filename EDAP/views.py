cf51c8a2211d77c5c4ff94c49d6b59fabceb3024
cuttlin <847612460@qq.com>
2021-02-26 22:58:33 +0800

    add world map

--- EDAP/views.py
+++ EDAP/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,HttpResponse
 from django.db.models import Max
 # Create your views here.
-from .models import City, Leiji, Yiqingv2
+from .models import City, Leiji, Yiqingv2, Leijiworld, country_name_map, Leijitwomonth
 
 
 def hello(request):
@@ -17,11 +17,20 @@ def service(request):
     return render(request,'service.html')
 
 def home(request):
+    leijitwomonth = Leijitwomonth.objects.order_by('-timestamp')[0]
     leiji = Leiji.objects.order_by('-timestamp')[0]
     context = {}
     yiqingv2 = Yiqingv2.objects.order_by('-timestamp')[0]
     dl = yiqingv2.dataList
-    return render(request,'home.html',{'leiji':leiji,'dl':dl})
+    return render(request,'home.html',{'leiji':leiji,'dl':dl,'lj2':leijitwomonth})
 
 def world(request):
-    return render(request,'world.html')
+    leijiworld = Leijiworld.objects.order_by('-timestamp')[0]
+    for item in leijiworld.countrydata['child']:
+        try:
+            item['name2'] = country_name_map[item['name2']]
+            #print(item['name2'])
+        except:
+            print()
+
+    return render(request,'world.html',{'leijiworld':leijiworld})
